a93cd5d078ade69820f8438bf32329e23fedb9c5
Jose Paredes <jparedes0303@gmail.com>
2021-02-10 20:59:18 -0500

    done

---
+++ script.js
@@ -0,0 +1,6 @@
+// let parallax = document.getElementById("parallax");
+
+// window.addEventListener("scroll", function (){
+//     let offset = window.pageYOffset;
+//     parallax.style.backgroundPositionY = offset * 0.7 + "px";
+// })
